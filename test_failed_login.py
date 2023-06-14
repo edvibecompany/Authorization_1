c9398a8b3eff3b9e163b425c6ee740635b857297
edvibecompany <edvibe.company@gmail.com>
2023-06-14 11:28:01 +0300

    add https://preview.edvibe.com

--- test_failed_login.py
+++ test_failed_login.py
@@ -1,7 +1,7 @@
 from selene import browser, have
 
 def test_login_false():
-    browser.open('https://edvibe.com/Account/Login')
+    browser.open('https://preview.edvibe.com/Account/Login')
     browser.element('.form-input [name=Email]').type('misha-marinov@mail.ru')
     browser.element('.form-input [name=Password]').type('password')
     browser.element('div.auth-form.auth-container > form > button').click()
--- test_teacher_sections.py
+++ test_teacher_sections.py
@@ -1,7 +1,7 @@
 from selene import browser, have, be
 
 def test_sections_check():
-    browser.open('https://edvibe.com/Account/Login')
+    browser.open('https://preview.edvibe.com/Account/Login')
     browser.element('.form-input [name=Email]').type('misha-marinov@mail.ru')
     browser.element('.form-input [name=Password]').type('liveUT00mPE8CB7Z').press_enter()
     browser.wait_until(browser.element('div.auth-form.auth-container > form > button:nth-child(4)').should(be.visible))
